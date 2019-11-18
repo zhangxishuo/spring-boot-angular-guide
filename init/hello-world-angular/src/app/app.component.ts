044a5129683a9e4ae6291902daa1b12c7bc4de62
zhangxishuo <zhangxishuo1998@gmail.com>
2019-11-18 18:09:38 +0800

    spring-json

--- init/hello-world-angular/src/app/app.component.ts
+++ init/hello-world-angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+
+  constructor(private httpClient: HttpClient) {
+    this.httpClient.get('http://127.0.0.1:8080/helloWorld')
+      .subscribe(this.success, this.error);
+  }
+
   title = 'hello-world';
+
+  success(data) {
+    console.log('请求成功');
+    console.log(data);
+  }
+
+  error(data) {
+    console.log('请求失败');
+    console.log(data);
+  }
 }
--- init/hello-world-spring-boot/src/main/java/club/yunzhi/helloWorld/HelloWorldApplication.java
+++ init/hello-world-spring-boot/src/main/java/club/yunzhi/helloWorld/HelloWorldApplication.java
@@ -2,20 +2,25 @@ package club.yunzhi.helloWorld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.Map;
+
+@CrossOrigin(origins = "*")
 @RestController
 @SpringBootApplication
 public class HelloWorldApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
 
-	@RequestMapping("helloWorld")
-	public String helloWorld() {
-		return "Hello Spring!";
-	}
+    @RequestMapping("helloWorld")
+    public Map<String, String> helloWorld() {
+        return Collections.singletonMap("message", "Hello Spring!");
+    }
 
 }
