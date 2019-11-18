c7268e566ff401b958027ed7150869f517ee0082
zhangxishuo <zhangxishuo1998@gmail.com>
2019-11-18 18:17:42 +0800

    observable

--- init/hello-world-angular/src/app/app.component.ts
+++ init/hello-world-angular/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, observable, Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,14 @@ export class AppComponent {
 
   constructor(private httpClient: HttpClient) {
     this.httpClient.get('http://127.0.0.1:8080/helloWorld')
+      .subscribe(this.success, this.error);
+
+    const yunzhi = new Yunzhi();
+
+    yunzhi.get('a')
+      .subscribe(this.success, this.error);
+
+    yunzhi.get('b')
       .subscribe(this.success, this.error);
   }
 
@@ -23,5 +32,24 @@ export class AppComponent {
   error(data) {
     console.log('请求失败');
     console.log(data);
+  }
+}
+
+class Yunzhi {
+  constructor() {
+  }
+
+  get(param: string): Observable<string> {
+    const subject = new Subject<string>();
+    setTimeout(() => {
+      if (param === 'a') {
+        subject.next('hello success');
+      } else {
+        subject.error('hello error');
+      }
+      subject.complete();
+    }, 100);
+
+    return subject;
   }
 }
