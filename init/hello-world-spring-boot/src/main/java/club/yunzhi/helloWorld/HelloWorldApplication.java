46be0081bbe794c5b5683eaa0cd9398b71c3e69f
zhangxishuo <zhangxishuo1998@gmail.com>
2019-11-18 17:21:12 +0800

    hello-spring

--- init/hello-world-spring-boot/src/main/java/club/yunzhi/helloWorld/HelloWorldApplication.java
+++ init/hello-world-spring-boot/src/main/java/club/yunzhi/helloWorld/HelloWorldApplication.java
@@ -2,12 +2,20 @@ package club.yunzhi.helloWorld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+	@RequestMapping("helloWorld")
+	public String helloWorld() {
+		return "Hello Spring!";
 	}
 
 }
