dc912f3c15862f2bad5ac82306bc64bfc06d6c1f
zhangxishuo <zhangxishuo1998@gmail.com>
2019-11-19 20:50:44 +0800

    jdbcTemplate

---
+++ api/src/main/java/com/mengyunzhi/springBootStudy/Teacher.java
@@ -0,0 +1,70 @@
+package com.mengyunzhi.springBootStudy;
+
+public class Teacher {
+    private Long id;
+    private String name;
+    private Boolean sex;
+    private String username;
+    private String email;
+    private Long createTime;
+    private Long updateTime;
+
+    public Teacher() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getSex() {
+        return sex;
+    }
+
+    public void setSex(Boolean sex) {
+        this.sex = sex;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Long createTime) {
+        this.createTime = createTime;
+    }
+
+    public Long getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Long updateTime) {
+        this.updateTime = updateTime;
+    }
+}
--- api/src/main/java/com/mengyunzhi/springBootStudy/TeacherController.java
+++ api/src/main/java/com/mengyunzhi/springBootStudy/TeacherController.java
@@ -1,41 +1,50 @@
 package com.mengyunzhi.springBootStudy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("teacher")
 public class TeacherController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TeacherController.class);
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @GetMapping
-    public Test getAll() {
-        return new Test(1, "test");
-    }
+    public List<Teacher> getAll() {
+        List<Teacher> teachers = new ArrayList<>();
+        RowCallbackHandler handler = (resultSet) -> {
+            Teacher teacher = new Teacher();
+            /*获取字段id，并转换为Long类型返回*/
+            teacher.setId(resultSet.getLong("id"));
+            /*获取字段name，并转换为String类型返回*/
+            teacher.setName(resultSet.getString("name"));
+            /*获取字段sex，并转换为布尔类型返回*/
+            teacher.setSex(resultSet.getBoolean("sex"));
+            teacher.setUsername(resultSet.getString("username"));
+            teacher.setEmail(resultSet.getString("email"));
+            teacher.setCreateTime(resultSet.getLong("create_time"));
+            teacher.setUpdateTime(resultSet.getLong("update_time"));
 
-    class Test {
-        private int id;
-        private String name;
+            /*将得到的teacher添加到要返回的数组中*/
+            teachers.add(teacher);
+        };
 
-        public Test(int id, String name) {
-            this.id = id;
-            this.name = name;
-        }
+        String query = "select id, name, sex, username, email, create_time, update_time from teacher";
 
-        public int getId() {
-            return id;
-        }
+        jdbcTemplate.query(query, handler);
 
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
+        return teachers;
     }
 }
