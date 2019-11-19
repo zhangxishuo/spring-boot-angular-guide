27f4bc582d2b192ea4394267678983d573d1b77b
zhangxishuo <zhangxishuo1998@gmail.com>
2019-11-19 20:28:25 +0800

    2.2.2

---
+++ api/src/main/java/com/mengyunzhi/springBootStudy/TeacherController.java
@@ -0,0 +1,41 @@
+package com.mengyunzhi.springBootStudy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("teacher")
+public class TeacherController {
+
+    @GetMapping
+    public Test getAll() {
+        return new Test(1, "test");
+    }
+
+    class Test {
+        private int id;
+        private String name;
+
+        public Test(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
